9aa99033463071d67542ea6a8374f7ec143703e2
Calamarify <16755396+tac0810@users.noreply.github.com>
2023-12-15 23:29:35 +0900

    Create vertexShader.glsl

---
+++ src/glsl/vertexShader.glsl
@@ -0,0 +1,16 @@
+precision mediump float;
+precision mediump int;
+
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+
+attribute vec3 position;
+attribute vec4 color;
+attribute vec2 uv;
+
+varying vec2 vUv;
+
+void main()	{
+	vUv = uv;
+	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
